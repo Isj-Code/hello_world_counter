db798aa3630338dd99884d482e2f928eb934a828
Isj-Code <isj.code@gmail.com>
2024-05-09 16:42:14 +0200

    Fix imports

--- lib/presentation/screens/counter/counter_functions_screen.dart
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CounterFunciontsScreen extends StatefulWidget {
   const CounterFunciontsScreen({super.key});
